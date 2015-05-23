5b86c26feba7b5983e97a028b0e6a67f4b306d61
omotee <tayo_babalola@outlook.com>
2015-05-23 20:49:27 +0100

    A new update

--- BasicAutomation/src/repoPractice/TeamBee.java
+++ BasicAutomation/src/repoPractice/TeamBee.java
@@ -2,6 +2,14 @@ package repoPractice;
 
 public class TeamBee {
 	
+	public static void practice(){
+		
+		System.out.println("this is  testing my repo");
+	}
 	
-
+	
+	public static void test(){
+		
+		System.out.println("Its all about testing");
+	}
 }
---
+++ BasicAutomation/src/repoPractice/TestTeamBee.java
@@ -0,0 +1,13 @@
+package repoPractice;
+
+public class TestTeamBee {
+
+	public static void main(String[] args) {
+		
+		TeamBee check = new TeamBee();
+		check.practice();
+		check.test();
+		
+	}
+
+}
