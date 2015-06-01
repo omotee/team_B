1f183225d28b427697cb42eb460ff855f3726f07
omotee <tayo_babalola@outlook.com>
2015-06-01 17:49:59 +0100

    Second Automation

---
+++ SecondAutomation/src/secondPackage/TestSecondWebAutomation.java
@@ -0,0 +1,11 @@
+package secondPackage;
+
+public class TestSecondWebAutomation {
+
+	public static void main(String[] args) {
+		SecondWebAutomation test = new SecondWebAutomation();
+		test.personal();
+
+	}
+
+}
