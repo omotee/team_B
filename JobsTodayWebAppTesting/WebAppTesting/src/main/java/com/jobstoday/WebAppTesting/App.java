db2836a6bbcebd974052c12bcdeb55e41e14ccc9
Ijeoma Nwofor <ijeoma.nwofor@gmail.com>
2015-06-10 23:24:40 +0100

    New Project

---
+++ JobsTodayWebAppTesting/WebAppTesting/src/main/java/com/jobstoday/WebAppTesting/App.java
@@ -0,0 +1,13 @@
+package com.jobstoday.WebAppTesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
