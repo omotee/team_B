f542b2861606e2eed9f625d86b9538aaaf9d1ff4
omotee <tayo_babalola@outlook.com>
2015-06-10 19:46:10 +0100

    Sports Direct Automation

---
+++ SportsDirect/SportsDirect/online.sportsdirect/src/main/java/sportsdirect/AdidasSambaPage.java
@@ -0,0 +1,12 @@
+package sportsdirect;
+
+import org.openqa.selenium.WebDriver;
+
+public class AdidasSambaPage extends AbstractPage {
+
+	public AdidasSambaPage(WebDriver driver) {
+		super(driver);
+		
+	}
+
+}
