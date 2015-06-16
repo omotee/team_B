580cd4d373ef3223e400b5ea2813296fb35e4ff6
Ijeoma Nwofor <ijeoma.nwofor@gmail.com>
2015-06-16 16:53:34 +0100

    Update

--- PuppyAdoptonWebAppTesting/com.puppies/src/main/java/com/puppies/main/AbstractPage.java
+++ PuppyAdoptonWebAppTesting/com.puppies/src/main/java/com/puppies/main/AbstractPage.java
@@ -4,9 +4,10 @@ import org.openqa.selenium.WebDriver;
 
 public class AbstractPage {
 	protected WebDriver driver;
-	private String baseUrl = "http://puppies.herokuapp.com/";
+	private String baseUrl = "http://puppies.herokuapp.com/";   
 
 	public AbstractPage(WebDriver driver) {
+		
 		this.driver = driver;
 	}
 	
